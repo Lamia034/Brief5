f1c9ba38de6ac3cc6f6f6c785681c7bc3a2d27e0
Lamia BOURICH <lamiascience034@gmail.com>
2023-10-27 11:05:15 +0100

    testing

--- src/test/java/com/bank/Service/CreditServiceTest.java
+++ src/test/java/com/bank/Service/CreditServiceTest.java
@@ -1,46 +1,86 @@
 package com.bank.Service;
 
-import com.bank.DAO.CreditDAO;
+
 import com.bank.DAO.CreditDAOImpl;
+import com.bank.Entity.Agency;
+import com.bank.Entity.Client;
 import com.bank.Entity.Credit;
+import com.bank.Entity.Employee;
 import com.bank.Enum.CreditStatus;
-import org.junit.jupiter.api.BeforeAll;
+import com.bank.Service.AgencyService;
+import com.bank.Service.ClientService;
+import com.bank.Service.CreditService;
+import com.bank.Service.EmployeeService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalTime;
+import java.util.Optional;
 
-class CreditServiceTest {
-    private static CreditDAOImpl creditDaoImpl;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-    @BeforeAll
-    public static void setUp() {
+@ApplicationScoped
+class CreditDAOImplTest {
 
-        creditDaoImpl = new CreditDAOImpl();
+    @InjectMocks
+    private CreditService creditService;
+
+    @Mock
+    private EntityManager entityManager;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void findByDate() {
-        try {
-            List<Credit> list = creditDaoImpl.findByDate(LocalDate.of(2023, 11, 11));
-            Assertions.assertEquals(1, list.size());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    void createCredit() throws Exception {
+        Credit credit = new Credit();
+        Agency agency = new Agency();
+        agency.setCode("agency1");
+        Client client = new Client();
+        client.setCode(5);
+        Employee employee = new Employee();
+        employee.setRegistrationNbr(1);
+
+        credit.setValue(555);
+        credit.setStatus(CreditStatus.PENDING);
+        credit.setDuration(25);
+        credit.setRemark("Remark");
+
+        credit.setAgency(agency);
+        credit.setClient(client);
+        credit.setEmployee(employee);
+        credit.setModification_date(LocalDate.parse("2020-02-20"));
+        credit.setModification_time(LocalTime.parse("11:11:11"));
+
+        when(entityManager.persist(any(Credit.class))).thenReturn(credit);
+
+        Optional<Credit> result = creditService.addCredit(credit);
+
+        assertEquals(credit, result.orElse(null));
     }
 
     @Test
-    void findByStatus() {
-        String statusToFind = "APPROVED";
+    void updateCredit() {
+        Credit credit = new Credit();
+        credit.setStatus(CreditStatus.PENDING);
+        CreditStatus newStatus = CreditStatus.REFUSED;
+        when(entityManager.find(Credit.class, 30)).thenReturn(credit);
 
-        List<Credit> credits = creditDaoImpl.findByStatus(statusToFind);
+        Optional<Credit> result = creditService.updateStatus(30, newStatus);
 
-        Assertions.assertNotNull(credits);
-        Assertions.assertFalse(credits.isEmpty());
-        for (Credit credit : credits) {
-            Assertions.assertEquals(CreditStatus.valueOf(statusToFind), credit.getStatus());
-        }
+        assertTrue(result.isPresent());
+        assertEquals(newStatus, result.get().getStatus());
     }
-    }
+}
 
